00aa5f7de87cd2f3f350f4505af9c8b04f86ee40
Ravi Shankar Yadav <116423641+raviiiyadav@users.noreply.github.com>
2022-11-01 00:50:00 +0530

    Add files via upload

--- Static/recommend.js
+++ Static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
     source.addEventListener('input', inputHandler);
   
     $('.movie-button').on('click',function(){
-      var my_api_key = '7fee067b60fd14ed0bd0013b0863045f';
+      var my_api_key = 'd493847c4845998f1e7cc1254607958d';
       var title = $('.movie').val();
       if (title=="") {
         $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
   
   // will be invoked when clicking on the recommended movies
   function recommendcard(e){
-    var my_api_key = '7fee067b60fd14ed0bd0013b0863045f';
+    var my_api_key = 'd493847c4845998f1e7cc1254607958d';
     var title = e.getAttribute('title'); 
     load_details(my_api_key,title);
   }
